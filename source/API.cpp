e4c3d1a6b91253741d815bb908dcb189c23588d5
Mateusz Kordowski <mateuszkd67@gmail.com>
2021-01-06 14:15:43 +0100

    Dodano autora

--- source/API.cpp
+++ source/API.cpp
@@ -73,6 +73,10 @@ int API::mynfs_lseek(int fd, int whence, int offset, FDManager& manager){
 
 }
 
+/**
+ * @author Mateusz Kordowski
+ * 
+ */
 int API::mynfs_close(int fd, FDManager& manager){
     logStart();
     if(!manager.exist(fd)) {
@@ -98,6 +102,10 @@ int API::mynfs_close(int fd, FDManager& manager){
     return 0;
 }
 
+/**
+ * @author Mateusz Kordowski
+ * 
+ */
 int API::mynfs_unlink(char* path, FDManager& manager){
     logStart();
     if(!checkPathLength(path)){
